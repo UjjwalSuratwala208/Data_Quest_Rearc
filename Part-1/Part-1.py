9f59f07ac154e32be37d4cba6aaf579f9c771d9a
UjjwalSuratwala208 <Saraswatichandr@123>
2022-10-14 02:43:52 -0400

    Fianl Commit for Part-1 and Part-2

--- Part-1/Part-1.py
+++ Part-1/Part-1.py
@@ -1,63 +1,87 @@
-#Script to fecth file
+#Script for fecthing files from URL
 
-# imoport necesarry libraries
+# import libraries
 
 import requests
 from bs4 import BeautifulSoup
+import os
+import boto3
 
-
+upload_file_bucket = 'dataquest-rearc'
 # reading the URL
 url = 'https://download.bls.gov/pub/time.series/pr/'
-print("URL",url)
-
 
 # Requests URL and get response object
 response = requests.get(url)
-print(" \n Responses",response)
 
 # Parse text obtained
 soup = BeautifulSoup(response.text, 'html.parser')
-print("parsed text \n", soup)
 
 # Find all hyperlinks present on webpage
 for links in soup.find_all('a'):
-
     fileurl = url+links.contents[0]
-    print("file Url \n", fileurl)
+    # print("file Url \n", fileurl)
 
+    fileurl.split('/')[-1]
     filename =links.contents[0]
-    print("filename \n",  links.contents[0])
-
-    r = requests.get(fileurl)
-    with open('data/'+filename,'wb') as f:
-        f.write(r.content)
 
 
+    if not '[To Parent Directory]' == str(filename) or not '[To Parent Directory]' in fileurl or not fileurl.split('/')[-1]=='[To Parent Directory]':
+        r = requests.get(fileurl)
+        if 'Current' in filename or 'AllData' in filename:
+            filename+='.csv'
+            print('Fileurl:', fileurl, 'Filename', filename)
+        with open('data/'+filename,'wb') as f:
+            f.write(r.content)
 
 # uploading the file using Boto3 into s3
-# import necessary packages
-import boto3
-import os
 
 # specify S3 service
 client = boto3.client('s3')
 
-
-
 #  uploading the file into s3 bucket
 path = os.listdir('data/')
 
+def isModified(bucket, key, fname):
+    s3 = boto3.resource('s3')
+    try:
+        obj = s3.Object(bucket, key)
+        return int(obj.content_length) != int(os.path.getsize(fname)) # if file size is diff or file not found =, then upload into s3
+    except Exception as e:
+        print('Key not present')
+        return True
 for file in path:
   #  print(file)
     full_path = "data/"+file
     with open(full_path, 'rb') as f:
 
-        upload_file_bucket = 'dataquest-rearc'
-        upload_file_key = 'part-1/  data/' + str(file)
-        print(upload_file_bucket, upload_file_key, file)
-        client.upload_fileobj(f, upload_file_bucket, upload_file_key)
+
+        upload_file_key = 'part-1/data/' + str(file)
+
+        # check if file changed
+        if isModified(upload_file_bucket,upload_file_key,full_path):
+            print(upload_file_bucket, upload_file_key, file)
+            client.upload_fileobj(f, upload_file_bucket, upload_file_key)
 
 
+# compare and delete data from s3 for file deleted from website
+local=os.listdir('data/')
+s3_list=client.list_objects_v2(Bucket=upload_file_bucket,Prefix='part-1/data/')
+s3_list=[obj['Key'].split('/')[-1] for obj in s3_list['Contents']]
+print('local_list',local)
+print('s3_list',s3_list)
+res = set(local)-set(s3_list)
+# Deletes the file from s3 that is removed from source
+for file in list(res):
+    response = client.delete_object(
+        Bucket=upload_file_bucket,
+        Key='part-1/data/'+file
+    )
 
-
-
+# delete data from local
+for file in os.listdir("data/"):
+    file='data/'+file
+    if os.path.exists(file):
+        os.remove(file)
+    else:
+        print("The file does not exist")
--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # Data_Quest_Rearc
+
+This quest is a good way to get the idea and learn more about what skill set will be needed to build and design the Data pipeline architecture.  
+
+PART-1 :
+--------------
+- This part is about the showing the Managing the data fom the different sources via utilizing the AWS S3 and 
+implementing in python.
+
+- US.Bureau of Labour Statistics  - time series dataset  was used for republishing and to store in s3.
+
+- `Part-1.py` python script file to process  the data from website and upload the file got from website in S3 and also keep in sync with source when the data on the website is updated, deleted or added. 
+
+
+- Steps for execution:-
+-------------------------
+
+1) Run the Script `Part-1.py` to extract the file form the link for bls.gov/pub.time series/pr link.
+2) Upload the files we got from above mentioned the links into a local directory.
+3) Upload all the files into s3 using Boto3.
+ 
+- Command to execute PART-1: 
+--------------------------------
+- Command: `0 22 * * * .... python Part-1.py` 
+
+CRON Expression: 
+-- It wll run every 0 minute of hour 22. 
+It will execute `Part-1.py` 
+
+
+PART-2 :
+--------------
+- In this section, mainly  focusing on working with the API for sourcing the data and to store it in-house AWS S3.
+
+- The Data USA API is used which has information related to the Population data. I have created the script Population_API.py to fecth the data.  
+
+
+- Steps for execution:
+----------------------
+
+1) Run the `Population_API.py` to fetch the data from the API and stored the result in a JSON
+2) Upload the JSON file in S3 - create new bucket in S3
+3) Using AWS CLI to upload the JSON file in to the S3 bucket.
+
+
+- Command to execute PART-2: 
+--------------------------------
+
+1) `AWS S3 sync Local to S3 bucket` – Copying/Uploading files
+2) After the uploading, execute `aws s3 ls`  to see the list of uploaded files. 
+
+- Command to execute PART-2: 
+--------------------------------
+- Command: `0 22 * * * .... python Population_API.py` 
+
+CRON Expression: 
+-- It wll run every 0 minute of hour 22. 
+It will execute `Part-1.py` 
+
+
+Note: For Both Part-1 and Part-2, This will be hosted on EC2 instance. 
